6b07514db79a32b2543db02e65a7c61814570350
Jon Seed <jonathan.j.seed@gmail.com>
2020-12-13 01:28:30 +0000

    Fix id encoding regex
    Add global flags

--- functions/tracks.js
+++ functions/tracks.js
@@ -6,7 +6,7 @@ const credentials = new AWS.Credentials(process.env.BUCKET_ACCESS_KEY, process.e
 const s3 = new AWS.S3({ endpoint, credentials });
 
 // TODO - decode [source: https://gist.github.com/jhurliman/1250118/09d7e584ddba7f7c37fd028d2d704497c06d1916]
-const encodeId = (val) => new Buffer(val).toString('base64').replace('+', '-').replace('/', '_').replace(/=+$/, '');
+const encodeId = (val) => new Buffer(val).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '');
 
 const returnStatus = (statusCode) => ({ statusCode });
 const successResponse = (body) => ({ ...returnStatus(200), body: JSON.stringify(body)});
